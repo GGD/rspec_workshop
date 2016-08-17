f8b02cda497c9844f4160646f7d4a059b1ff1165
GaDii Jian <ggd747@gmail.com>
2016-08-17 14:38:40 +0800

    refactor

--- spec/controllers/api/v1/posts_controller_spec.rb
+++ spec/controllers/api/v1/posts_controller_spec.rb
@@ -4,23 +4,29 @@ module API
   module V1
     describe PostsController do
       describe 'GET #index' do
-        it 'returns posts' do
-          first_post = Post.create(title: 'first post', content: 'content')
-          second_post = Post.create(title: 'second post', content: 'content')
+        subject { get :index }
 
-          get :index
+        before do
+          Post.create(title: 'first post', content: 'content')
+          Post.create(title: 'second post', content: 'content')
+        end
+
+        it 'returns posts' do
+          subject
 
           expect(response).to have_http_status 200
-          post_ids = json_response.map { |post| post[:id] }
-          expect(post_ids).to contain_exactly(first_post.id, second_post.id)
+          post_titles = json_response.map { |post| post[:title] }
+          expect(post_titles).to contain_exactly('first post', 'second post')
         end
       end
 
       describe 'GET #show' do
-        it 'returns the post' do
-          post = Post.create(title: 'title', content: 'content')
+        subject { get :show, params: { id: post.id } }
 
-          get :show, params: { id: post.id }
+        let(:post) { Post.create(title: 'title', content: 'content') }
+
+        it 'returns the post' do
+          subject
 
           expect(response).to have_http_status 200
           expect(json_response[:id]).to eq post.id
@@ -28,11 +34,13 @@ module API
       end
 
       describe 'POST #create' do
+        subject { post :create, params: { post: post_params } }
+
+        let(:post_params) { { title: 'my title', content: 'content' } }
+
         context 'with valid params' do
           it 'returns the created post' do
-            post_params = { title: 'my title', content: 'content' }
-
-            post :create, params: { post: post_params }
+            subject
 
             expect(response).to have_http_status 201
             expect(json_response[:title]).to eq 'my title'
@@ -40,10 +48,10 @@ module API
         end
 
         context 'with invalid params' do
-          it 'returns errors' do
-            post_params = { title: 'my title', content: '' }
+          let(:post_params) { { title: 'my title', content: '' } }
 
-            post :create, params: { post: post_params }
+          it 'returns errors' do
+            subject
 
             expect(response).to have_http_status 422
             expect(json_response[:content]).to include "can't be blank"
@@ -52,12 +60,14 @@ module API
       end
 
       describe 'PUT #update' do
+        subject { put :update, params: { id: post.id, post: post_params } }
+
+        let(:post) { Post.create(title: 'title', content: 'content') }
+        let(:post_params) { { title: 'new title', content: post.content } }
+
         context 'with valid params' do
           it 'returns the post' do
-            post = Post.create(title: 'title', content: 'content')
-            post_params = { title: 'new title', content: post.content }
-
-            put :update, params: { id: post.id, post: post_params }
+            subject
 
             expect(response).to have_http_status 200
             expect(json_response[:title]).to eq 'new title'
@@ -65,11 +75,10 @@ module API
         end
 
         context 'with invalid params' do
-          it 'returns errors' do
-            post = Post.create(title: 'title', content: 'content')
-            post_params = { title: '', content: post.content }
+          let(:post_params) { { title: '', content: post.content } }
 
-            put :update, params: { id: post.id, post: post_params }
+          it 'returns errors' do
+            subject
 
             expect(response).to have_http_status 422
             expect(json_response[:title]).to include "can't be blank"
@@ -78,10 +87,12 @@ module API
       end
 
       describe 'DELETE #destroy' do
-        it 'returns nothing' do
-          post = Post.create(title: 'title', content: 'content')
+        subject { delete :destroy, params: { id: post.id } }
 
-          delete :destroy, params: { id: post.id }
+        let(:post) { Post.create(title: 'title', content: 'content') }
+
+        it 'returns nothing' do
+          subject
 
           expect(response).to have_http_status 204
           expect(response.body).to be_blank
