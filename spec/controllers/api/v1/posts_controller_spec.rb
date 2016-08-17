8013c379fcfd49c54d8b1135b96f0248c3bb92c1
GaDii Jian <ggd747@gmail.com>
2016-08-17 12:56:56 +0800

    implement posts#index, posts#update, posts#destroy

--- spec/controllers/api/v1/posts_controller_spec.rb
+++ spec/controllers/api/v1/posts_controller_spec.rb
@@ -3,6 +3,19 @@ require 'rails_helper'
 module API
   module V1
     describe PostsController do
+      describe 'GET #index' do
+        it 'returns posts' do
+          first_post = Post.create(title: 'first post', content: 'content')
+          second_post = Post.create(title: 'second post', content: 'content')
+
+          get :index
+
+          expect(response).to have_http_status 200
+          post_ids = json_response.map { |post| post[:id] }
+          expect(post_ids).to contain_exactly(first_post.id, second_post.id)
+        end
+      end
+
       describe 'GET #show' do
         it 'returns the post' do
           post = Post.create(title: 'title', content: 'content')
@@ -35,6 +48,43 @@ module API
             expect(response).to have_http_status 422
             expect(json_response[:content]).to include "can't be blank"
           end
+        end
+      end
+
+      describe 'PUT #update' do
+        context 'with valid params' do
+          it 'returns the post' do
+            post = Post.create(title: 'title', content: 'content')
+            post_params = { title: 'new title', content: post.content }
+
+            put :update, params: { id: post.id, post: post_params }
+
+            expect(response).to have_http_status 200
+            expect(json_response[:title]).to eq 'new title'
+          end
+        end
+
+        context 'with invalid params' do
+          it 'returns errors' do
+            post = Post.create(title: 'title', content: 'content')
+            post_params = { title: '', content: post.content }
+
+            put :update, params: { id: post.id, post: post_params }
+
+            expect(response).to have_http_status 422
+            expect(json_response[:title]).to include "can't be blank"
+          end
+        end
+      end
+
+      describe 'DELETE #destroy' do
+        it 'returns nothing' do
+          post = Post.create(title: 'title', content: 'content')
+
+          delete :destroy, params: { id: post.id }
+
+          expect(response).to have_http_status 204
+          expect(response.body).to be_blank
         end
       end
     end
