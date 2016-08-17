8f22feb3975bb1b400bd6159bfdf8bb2070bbe5b
GaDii Jian <ggd747@gmail.com>
2016-08-17 16:12:27 +0800

    implement test suite

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Post do
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:content) }
+
+
+  describe '#filter_sensitive_word' do
+    subject { described_class.new.send(:filter_sensitive_word) }
+
+    before { allow_any_instance_of(described_class).to receive(:do_something_really_takes_time) }
+
+    it 'returns blank string' do
+      expect(subject).to be_blank
+    end
+  end
 end
