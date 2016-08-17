20351ba5dce628dbe4021727ddb9bbe2b648ee75
GaDii Jian <ggd747@gmail.com>
2016-08-17 11:13:53 +0800

    boot up

---
+++ README.md
@@ -0,0 +1,6 @@
+# Preparation
+
+1. `git clone git@github.com:GGD/rspec_workshop`
+1. `bundle install` (we're using Rails `5.0.0` or `5.0.0.1`)
+1. `rails db:migrate`
+1. `rails server`
---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,48 @@
+module API
+  module V1
+    class PostsController < ApplicationController
+      def index
+        posts = Post.all
+        render status: 200, json: posts
+      end
+
+      def show
+        post = Post.find(params[:id])
+        render status: 200, json: post
+      end
+
+      def create
+        post = Post.new(post_params)
+
+        if post.save
+          render status: 201, json: post
+        else
+          render status: 422, json: post.errors
+        end
+      end
+
+      def update
+        post = Post.find(params[:id])
+
+        if post.update(post_params)
+          render status: 200, json: post
+        else
+          render status: 422, json: post.errors
+        end
+      end
+
+      def destroy
+        post = Post.find(params[:id])
+        post.destroy
+
+        render status: 204
+      end
+
+      private
+
+      def post_params
+        params.require(:post).permit(:title, :content)
+      end
+    end
+  end
+end
